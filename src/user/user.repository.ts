2e549de8784e0f5c14c45c3ee54c2d5be8e4ec5a
muadz <goadzaskarul@gmail.com>
2021-01-02 23:37:24 +0700

    add postgres error code library

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,6 +2,7 @@ import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 import { AuthCredentialDto } from 'src/auth/dto/auth.credentials.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { PG_UNIQUE_VIOLATION } from "@drdgvhbh/postgres-error-codes";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -15,7 +16,7 @@ export class UserRepository extends Repository<User> {
         try {
             await user.save();
         } catch (error) {
-            if (error.code === '23505') { // duplicate username field
+            if (error.code === PG_UNIQUE_VIOLATION) {
                 throw new ConflictException('Username already exist')
             } else {
                 throw new InternalServerErrorException()
