75aa7d1c775eba17eef0fff3904ca095353c1fac
muadz <goadzaskarul@gmail.com>
2021-02-25 17:05:00 +0700

    add auth constant

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,23 +3,24 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/user/user.repository';
+import authConstants from './auth-constants';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([UserRepository]),
-        JwtModule.register({
-            secret: 'someSecret',
-            signOptions: {
-                expiresIn: 3600,
-            },
-        }),
-        PassportModule.register({ defaultStrategy: 'jwt' }),
-    ],
-    controllers: [AuthController],
-    providers: [AuthService, JwtStrategy],
-    exports: [JwtStrategy, PassportModule],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    JwtModule.register({
+      secret: authConstants.jwt.secret,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
